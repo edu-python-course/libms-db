3a5500e0c3883d4f059da5101772dfc8dacdd88a
Serhii Horodilov <sgorodil@gmail.com>
2023-09-30 16:06:50 +0300

    Alter tables owner

--- libms.sql
+++ libms.sql
@@ -13,11 +13,17 @@ CREATE TABLE publisher
     id SERIAL PRIMARY KEY
 );
 
+ALTER TABLE publisher
+    OWNER TO libms;
+
 -- label: ddl-author
 CREATE TABLE author
 (
     id SERIAL PRIMARY KEY
 );
+
+ALTER TABLE author
+    OWNER TO libms;
 
 -- label: ddl-book
 CREATE TABLE book
@@ -25,11 +31,17 @@ CREATE TABLE book
     id SERIAL PRIMARY KEY
 );
 
+ALTER TABLE book
+    OWNER TO libms;
+
 -- label: ddl-book_author
 CREATE TABLE book_author
 (
     id SERIAL PRIMARY KEY
 );
+
+ALTER TABLE book_author
+    OWNER TO libms;
 
 -- label: ddl-member
 CREATE TABLE member
@@ -37,14 +49,23 @@ CREATE TABLE member
     id SERIAL PRIMARY KEY
 );
 
+ALTER TABLE member
+    OWNER TO libms;
+
 -- label: ddl-revenue
 CREATE TABLE revenue
 (
     id SERIAL PRIMARY KEY
 );
 
+ALTER TABLE revenue
+    OWNER TO libms;
+
 -- label: ddl-borrow_request
 CREATE TABLE borrow_request
 (
     id SERIAL PRIMARY KEY
 );
+
+ALTER TABLE borrow_request
+    OWNER TO libms;
